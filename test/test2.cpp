58cccc811f8d392f223a7cd667f4eef834d41f81
Daniil Shvalov <shvalovich@gmail.com>
2022-08-16 15:00:18 +0300

    feat(ci): add `--output-on-failure`

--- test/test2.cpp
+++ test/test2.cpp
@@ -8,5 +8,5 @@ TEST_CASE("testing the factorial") {
     CHECK(factorial2(1) == 1);
     CHECK(factorial2(2) == 2);
     CHECK(factorial2(3) == 6);
-    CHECK(factorial2(10) == 362880);
+    CHECK(factorial2(10) == 3628800);
 }
